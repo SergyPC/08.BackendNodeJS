4f48ead7d4ee8a2e5cf0054dca4e500d90aed940
Sergio Perez <sergy.perez@gmail.com>
2020-03-24 17:23:56 +0100

    Continuamos configurando la app y cambiamos las vistas de ejs a html.

--- app.js
+++ app.js
@@ -16,9 +16,13 @@ var app = express(); //Definimos la aplicación de Express
  * Configuramos la aplicación
  */
 // view engine setup (utilizando ejs)
-app.set('views', path.join(__dirname, 'views')); //Aquí le decimos a Express que cuando tenga que renderizar una vista, las vistas están en la carpeta ./views/
-app.set('view engine', 'ejs'); // Y que esas vistas están estructuradas con el motor de vistas ejs (También llamado motor de plantillas)
+//app.set('views', path.join(__dirname, 'views')); //Aquí le decimos a Express que cuando tenga que renderizar una vista, las vistas están en la carpeta ./views/
+//app.set('view engine', 'ejs'); // Y que esas vistas están estructuradas con el motor de vistas ejs (También llamado motor de plantillas)
 
+// view engine setup utilizando html
+app.set('views', path.join(__dirname, 'views')); //Aquí le decimos a Express que cuando tenga que renderizar una vista, las vistas están en la carpeta ./views/
+app.set('view engine', 'html'); // Y que esas vistas están estructuradas con el motor de vistas html (También llamado motor de plantillas)
+app.engine('html', require('ejs').__express); //Definimos un nuevo motor de vistas html
 
 // middlewares:
 app.use(logger('dev')); //Log
@@ -27,8 +31,8 @@ app.use(express.urlencoded({ extended: false })); //urlencoded para temas de POS
 app.use(cookieParser()); //Parsea las cookies
 app.use(express.static(path.join(__dirname, 'public'))); //Servir ficheros estáticos
 
-//Servir ficheros estáticos:
-//app.use('/pdf', express.static('d:/pdfs');
+// Servir ficheros estáticos:
+// app.use('/pdf', express.static('d:/pdfs'));
 
 
 // app.use( (req, res, next) => {
@@ -63,13 +67,43 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  if(err.array) { // error de validación (Añadido para las validaciones de express-validator)
+    err.status = 422; // ponemos el error Unprocessable Entity para que no devuleva el error 500 ya que la validación realizada en index (router.get('/enquerystring') no está devolviendo ningún error y la linea "res.status(err.status || 500);" si no hay error pone un 500
+    
+    /**
+     * onlyFirstError: true -> Solo cogemos el primer error para cada campo añadido en la validación del index (talla, color, piso, dirección, ...), añadiéndolo al array. (podríamos cogerlos todos).
+     * Ya que un campo puede tener varios errores de validación y sólo queremos pasar el primero que se genere 
+     * Por ejemplo, El campo Letra sólo puede ser letra y en mayusculas:
+     *  query('letra').isString().withMessage('debería ser numérico'),
+     *  query('letra').isUppercase().withMessage('debería ser numérico'),
+     * Si hubiera más validaciones de otros campos se iría añadiendo al array (err.array) el primer error encontrado (onlyFirstError).
+     */
+    const errInfo = err.array({ onlyFirstError: true })[0]; // err.array devuelve un array. Al poner [0] sólo cogemos el primer error que haya en ese array de errores.
+    
+    //err.message = `El parámetro ${errInfo.param} ${errInfo.msg}`;
+    err.message = isAPIRequest(req) ? 
+    { message: 'Not valid', errors: err.mapped() }
+    : `El parámetro ${errInfo.param} ${errInfo.msg}`;
+  }
+
+  res.status(err.status || 500);
+
+  if (isAPIRequest(req)) {
+    res.json({ error: err.message });
+    return;
+  }
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  // res.status(err.status || 500);
   res.render('error'); //renderizamos una página de error, mostrando el error: Llama a la vista ./views/error.ejs o html
 });
+
+function isAPIRequest(req) {
+  return req.originalUrl.startsWith('/api/'); //Devuelve todo menos el dominio (http://localhost:3000/api/zzz devuelve /api/zzz)
+}
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+//Validaciones En el middleware: Destructuring:
+const { query, check, validationResult } = require('express-validator');
+
 /* Middleware */
 // router.get y router.use es lo mismo
 // Ponerle .get es un filtro más: Es decirle a Express sólo cuando la petición sea de tipo GET y sea a la raiz de este middleware, entonces le pasas el request a este middleware que hay aquí
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' }); //renderiza la vista index y le vas a pasar el siguiente objeto ({...}) a la vista ./views/index.ejs o html
+  res.render('index', { title: 'Express' }); //renderiza la vista/pagina index y le vas a pasar el siguiente objeto ({...}) a la vista ./views/index.ejs o html
+});
+
+// Querystring con validaciones
+// npm i express-validator@4.0.0
+router.get('/enquerystring', [
+  query('talla').isNumeric().withMessage('debería ser numérico'),
+  //check('talla').isNumeric().withMessage('debería ser numérico'), //No sólo mira en el queryparams sino en todos los sitios donde pueda recibir ese parámetro
+  query('color').isString().withMessage('debería ser string'),
+  // query('color').custom (value => {
+  //   // añadir la logica para ese value
+  //   return true; // Es que valida
+  //   return false; // Es que no valida
+  // }),
+], (req, res, next) => { //http://localhost:3000/enquerystring?talla=L&color=rojo
+  // try {
+    validationResult(req).throw(); // lanza excepción si hay errores de validación
+    console.log("req.query:", req.query);
+    res.send('OK');
+  // } catch (err) {
+  //   next(err);
+  // }
 });
 
 module.exports = router;
